6faf04e1b933c5640d1ac454a1848ea377ce849b
Adam Beagle <abeagle88@gmail.com>
2014-11-19 16:16:31 -0500

    Added coordinate system blurb to game __init__

--- flippyflapwivs/game/__init__.py
+++ flippyflapwivs/game/__init__.py
@@ -1,7 +1,20 @@
 """
 platformer.game
 
-This subpackage contains all "pure" game logic, i.e. pygame is not touched.
+DESCRIPTION:
+  This subpackage contains all "pure" game logic, i.e. pygame is not touched.
+  The goal is to abstract out as much game logic as possible so the ui is less
+  cluttered with logic and more about merely visually representing the current
+  state of the game.
+
+COORDINATE SYSTEM:
+  The gamedata module assumes a coordinate system where the ranges of both
+  the x and y axes are [0, 1], where the extents represent the edges of the
+  visible area. It is assumed the positive direction of each mirrors Pygame,
+  i.e. the positive direction of x is "right" and that of y is "down."
+  All positions, constants, etc. in this subpackage use this
+  system. The ui package is expected to translate such values to screen
+  coordinates.
 """
 from .gamedata import GameData
 from .gamestate import GameState
